8776abe360dfb45a577dea85b0051c805d9964c9
Christian Mueller <derkaktus@mailbox.org>
2026-06-13 21:35:32 +0200

    Update __init__.py

--- components/enocean_mqtt_bridge/__init__.py
+++ components/enocean_mqtt_bridge/__init__.py
@@ -5,6 +5,7 @@ from esphome.const import CONF_ID, CONF_NAME
 
 DEPENDENCIES = ['uart', 'mqtt']
 
+# C++ Namespace und Klasse referenzieren
 enocean_ns = cg.esphome_ns.namespace('enocean_mqtt_bridge')
 EnOceanBridge = enocean_ns.class_('EnOceanBridge', cg.Component, uart.UARTDevice)
 
@@ -12,23 +13,30 @@ CONF_KNOWN_DEVICES = 'known_devices'
 CONF_DEVICE_ID = 'device_id'
 CONF_EEP = 'eep'
 
-# Schema für ein einzelnes Gerät
+# Schema für ein einzelnes Gerät aus der known_devices Liste
 DEVICE_SCHEMA = cv.Schema({
-    cv.Required(CONF_DEVICE_ID): cv.hex_uint32_t,
-    cv.Optional(CONF_NAME, default=""): cv.string,
-    cv.Optional(CONF_EEP, default=""): cv.string,
+    cv.Required(CONF_DEVICE_ID): cv.hex_int,
+    cv.Required(CONF_NAME): cv.string_strict,
+    cv.Required(CONF_EEP): cv.string_strict,
 })
 
+# Haupt-Schema für die Komponente
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(EnOceanBridge),
     cv.Optional(CONF_KNOWN_DEVICES, default=[]): cv.ensure_list(DEVICE_SCHEMA),
 }).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
 
+
 async def to_code(config):
+    # C++ Objekt instanziieren
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
 
-    # Geräte an C++ übergeben
+    # Bekannte Geräte aus der YAML dem C++ Objekt hinzufügen
     for device in config[CONF_KNOWN_DEVICES]:
-        cg.add(var.add_known_device(device[CONF_DEVICE_ID], device[CONF_NAME], device[CONF_EEP]))
+        cg.add(var.add_known_device(
+            device[CONF_DEVICE_ID], 
+            device[CONF_NAME], 
+            device[CONF_EEP]
+        ))
